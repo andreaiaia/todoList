747eb1bd25126896ae0a38e707712cd7362241a2
Andrea Bianchi <bianan96@gmail.com>
2020-07-18 10:01:48 +0200

    add methods to the todo and project factories

--- src/script.js
+++ src/script.js
@@ -13,12 +13,23 @@ const todoFactory = (title, dueDate, priority, description, notes) => {
   const sayDescription = () => {return _description};
   const sayNotes = () => {return _notes};
 
+  const editTitle = (newTitle) => {_title = newTitle};
+  const editDueDate = (newDueDate) => {_dueDate = newDueDate};
+  const editPriority = (newPriority) => {_priority = newPriority};
+  const editDescription = (newDescription) => {_description = newDescription};
+  const editNotes = (newNotes) => {_notes = newNotes};
+
   return {
     sayTitle,
     sayDueDate,
     sayDescription,
     sayPriority,
     sayNotes,
+    editDescription,
+    editDueDate,
+    editNotes,
+    editPriority,
+    editTitle,
   }
 }
 // const _newTodo = todoFactory((all the variables needed));
@@ -31,21 +42,23 @@ const projectFactory = (title, color) => {
   const sayTitle = () => {return _title};
   const sayColor = () => {return _color};
 
+  const editTitle = (newTitle) => {_title = newTitle};
+  const editColor = (newColor) => {_Color = newColor};
+
   return {
     sayTitle,
     sayColor,
+    editTitle,
+    editColor,
   }
 }
 // const _newProject = projectFactory((all the variables needed));
 // database[_newProject] = _newProject;
 
-var database = {
-  default:{
-    esempio:{
-      dueDate : "12/12/20",
-      priority : 5,
-      description : "this is an example",
-      notes : "here you can put any extra information that is needed for the Todo"
-    },
-  },
-};
+const database = {};
+
+const _newProject = projectFactory('default', 'red');
+database["default"] = _newProject;
+
+const _newTodo = todoFactory("example", "15/08/2020", 5, "trying the functions", "please work!");
+database["default"]["_newTodo"] = _newTodo;
